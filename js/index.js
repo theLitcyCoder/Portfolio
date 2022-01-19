5b7b89ad5ca9ff009f7b9857e4d3c048a796b194
Alicia A Harewood <45209768+theLitcyCoder@users.noreply.github.com>
2022-01-19 10:19:42 -0400

    portoflioandcalculator

---
+++ js/index.js
@@ -0,0 +1,31 @@
+const navToggle = document.querySelector('.nav-toggle');
+const navLinks = document.querySelectorAll('.nav_link');
+const mybutton = document.getElementById("page-up");
+
+navToggle.addEventListener('click', () => {
+    document.body.classList.toggle('nav-open');
+})
+
+navLinks.forEach(link => {
+    link.addEventListener('click', () => {
+        document.body.classList.remove('nav-open');
+    })
+})
+
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 105 || document.documentElement.scrollTop > 10) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+mybutton.addEventListener('click', () => {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+})
