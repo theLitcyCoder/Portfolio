683a6b881c2e4ec6ce1224ed9876c831ce594b97
Alicia Harewood <alicia.harewoodbcc@gmail.com>
2022-10-01 17:41:34 -0400

    scroll

--- js/index.js
+++ js/index.js
@@ -14,32 +14,32 @@ navLinks.forEach(link => {
 })
 
 
-abcap.addEventListener('click', () => {
-  Email.send({
-    Host: "smtp.elasticemail.com",
-    Username : "miwohamino@gmail.com",
-    Password : "FAE332F057D3910F00F44B246268A82D7D0E",
-    To : "miwohamino@gmail.com",
-    From : "miwohamino@gmail.com",
-    Subject : "Clicksss",
-    Body : "users on my website " ,
-    }).then(
-    );
-})
+// abcap.addEventListener('click', () => {
+//   Email.send({
+//     Host: "smtp.elasticemail.com",
+//     Username : "miwohamino@gmail.com",
+//     Password : "FAE332F057D3910F00F44B246268A82D7D0E",
+//     To : "miwohamino@gmail.com",
+//     From : "miwohamino@gmail.com",
+//     Subject : "Clicksss",
+//     Body : "users on my website " ,
+//     }).then(
+//     );
+// })
 
-// When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
+// // When the user scrolls down 20px from the top of the document, show the button
+// window.onscroll = function() {scrollFunction()};
 
-function scrollFunction() {
-  if (document.body.scrollTop > 105 || document.documentElement.scrollTop > 10) {
-    mybutton.style.display = "block";
-  } else {
-    mybutton.style.display = "none";
-  }
-}
+// function scrollFunction() {
+//   if (document.body.scrollTop > 105 || document.documentElement.scrollTop > 10) {
+//     mybutton.style.display = "block";
+//   } else {
+//     mybutton.style.display = "none";
+//   }
+// }
 
-// When the user clicks on the button, scroll to the top of the document
-mybutton.addEventListener('click', () => {
-    document.body.scrollTop = 0;
-    document.documentElement.scrollTop = 0;
-})
+// // When the user clicks on the button, scroll to the top of the document
+// mybutton.addEventListener('click', () => {
+//     document.body.scrollTop = 0;
+//     document.documentElement.scrollTop = 0;
+// })
